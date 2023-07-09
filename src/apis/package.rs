37714b38f56b14bd92df45b31868f1a2a5b9ad6c
yum <yumzi114@gmail.com>
2023-07-09 15:36:49 +0900

    d

--- src/apis/package.rs
+++ src/apis/package.rs
@@ -1,4 +1,4 @@
-use crate::{Error, Result,yumdb::packagedb::{manager_getdb,package_getdb,PackageManager}};
+use crate::{Error, Result,yumdb::packagedb::{manager_getdb,package_getdb,PackageManager,cmdlist_getdb}};
 use axum::{Json, routing::{Route, get}, Router,extract::Query};
 use serde::{Serialize, Deserialize};
 use serde_json::{Value, json};
@@ -26,6 +26,12 @@ struct PackageParams{
     name:Option<String>,
     description:Option<String>,
     manager_name:Option<String>,
+    // role:Option<UserRole::VIEWER(String::de)>
+}
+#[derive(Debug,Deserialize)]
+struct CmdParams{
+    name:Option<String>,
+    description:Option<String>,
     // role:Option<UserRole::VIEWER(String::de)>
 }
 async fn get_manager(Query(params):Query<ManagerParams>)->Result<Json<Value>>{
@@ -75,8 +81,29 @@ async fn get_package(Query(params):Query<PackageParams>)->Result<Json<Value>>{
         return Err(Error::DBNotFound);
     }
 }
+
+async fn get_cmd(Query(params):Query<CmdParams>)->Result<Json<Value>>{
+    if cfg!(debug_assertions){
+        println!("->> {:<12} - api_cmd","HANDLER");
+    }
+    let name = params.name.unwrap_or(String::from(""));
+    let description = params.description.unwrap_or(String::from(""));;
+
+    if let Ok(data) = cmdlist_getdb(name.as_str(),description.as_str()).await{
+        let result = json!({
+            "search_name":&name,
+            "description":&description,
+            "data":&data,
+            "result":true
+        });
+        return Ok(Json(result));
+    }else{
+        return Err(Error::DBNotFound);
+    }
+}
 pub fn routes()->Router{
     Router::new()
     .route("/api/manager", get(get_manager))
     .route("/api/package", get(get_package))
+    .route("/api/cmd", get(get_cmd))
 }
--- src/yumdb/packagedb.rs
+++ src/yumdb/packagedb.rs
@@ -46,6 +46,24 @@ impl Package{
         }
     }
 }
+#[derive(Debug, Deserialize,Serialize)]
+pub struct CmdList{
+    pub id:i32,
+    pub cmd:String,
+    pub description:String,
+}
+impl CmdList{
+    fn new (row:sqlx::postgres::PgRow)->Self{
+        let id =  row.get::<i32,_>("id");
+        let cmd =  row.get::<String,_>("cmd");
+        let description =  row.get::<String,_>("description");
+        Self { 
+            id, 
+            cmd, 
+            description, 
+        }
+    }
+}
 pub async fn manager_getdb(manager_name:&str, description:&str)->Result<Vec<PackageManager>,sqlx::Error>{
     dotenv().ok();
     let mut mutresult: Vec<PackageManager>= vec![];
@@ -84,23 +102,25 @@ pub async fn package_getdb(package_name:&str,manager_name:&str, description:&str
     format!("SELECT package.*,packagemanager.manager FROM package JOIN packagemanager ON package.manager_id = packagemanager.id WHERE package.package_name LIKE '%{}%' AND package.description LIKE '%{}%' AND packagemanager.manager LIKE '%{}%'", package_name,description,manager_name);
     let pgrow = sqlx::query(&query_str)
     .fetch_all(&pool).await?;
-    // println!("{:?}",pgrow[0].columns()[1]);
     for row in pgrow{
-        // let temp:String=row.get("package_name");
-        // println!("{:?}",temp);
         let data = Package::new(row);
         mutresult.push(data);
     }
     Ok(mutresult)
-    // let test = row.try_get::<i32,_>("role_id").unwrap();
-    // println!("row ????{:?}",test);
-    // let hash = ?;
-    // if let Ok(hash)=row.try_get::<String,_>("psw"){
-    //     let userinfo = PackageManager::new(row);
-    //     // let result = password_check(psw.to_string(),hash);
-    //     return Ok(userinfo);
-    // } else {
-    //     return Err(sqlx::Error::RowNotFound);
-    // }
-    // Ok(hash)
+}
+pub async fn cmdlist_getdb(cmd_name:&str, description:&str)->Result<Vec<CmdList>,sqlx::Error>{
+    dotenv().ok();
+    let mut mutresult: Vec<CmdList>= vec![];
+    let pg_url =env::var("PG_CONNETION").unwrap();
+    let pool = PgPoolOptions::new()
+        .connect(&pg_url)
+        .await?;
+    let query_str = format!("SELECT * FROM cmdlist WHERE cmd LIKE '%{}%' AND description LIKE '%{}%'", cmd_name,description);
+    let pgrow = sqlx::query(&query_str)
+    .fetch_all(&pool).await?;
+    for row in pgrow{
+        let data = CmdList::new(row);
+        mutresult.push(data);
+    }
+    Ok(mutresult) 
 }
